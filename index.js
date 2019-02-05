19a4cf6ae8b87744c64208c1cc8e9e33c3d39f62
kendall willard <kendallwillard@hotmail.com>
2019-02-05 02:33:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ function placeOrder(cardNumber) {
   }
   
   else {
+    viewCart()
    cart = []
   var totalCost = total()
    var result = `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`
